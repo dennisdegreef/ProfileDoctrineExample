ee6ac722e3f8289ba566f002a2a2416d14272c16
Dennis de Greef <github@link0.net>
2015-03-08 13:32:08 +0100

    Added Link0/ProfilingDoctrineBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Link0\ProfilingDoctrineBundle\Link0ProfilingDoctrineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Link0/ProfilingDoctrineBundle/Link0ProfilingDoctrineBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Link0\ProfilingDoctrineBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Link0ProfilingDoctrineBundle extends Bundle
+{
+}
